51c016288e395a36d4cf36b37fe90ab4e594ef7e
Charizza Thunjung <charizzathunjungsukmanaputra@mail.ugm.ac.id>
2023-10-12 08:58:20 +0700

    PPW2_UTS_A2_Charizza

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+Route::resource('products', ProductController::class);
